2ed0c690c42bfd7ad946175c7ee8c045d11c0cd0
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2020-09-06 15:58:57 +0100

    Converted some file elements - more need to be created first

--- utils/nightfire-model-converter/src/appmodel/XashModelFileWriter.cpp
+++ utils/nightfire-model-converter/src/appmodel/XashModelFileWriter.cpp
@@ -1,7 +1,30 @@
 #include "XashModelFileWriter.h"
+#include "elements/Conversions.h"
 
 namespace NFMDL
 {
+	template<typename T>
+	static inline void CopyElementArray(const ElementArray<T>& src, ElementArray<T>& dest)
+	{
+		dest.AllocateAndZero(src.Count());
+
+		src.ForEach([](uint32_t index, const T& element)
+		{
+			dest[index] = element;
+		});
+	}
+
+	template<typename TS, typename TD>
+	static inline void CopyElementArray(const ElementArray<TS>& src, ElementArray<TD>& dest)
+	{
+		dest.AllocateAndZero(src.Count());
+
+		src.ForEach([](uint32_t index, const TS& element)
+		{
+			Convert(element, dest[index]);
+		});
+	}
+
 	XashModelFileWriter::XashModelFileWriter(const std::shared_ptr<XashModelFile>& outModelFile) :
 		m_OutModelFile(outModelFile)
 	{
@@ -57,9 +80,25 @@ namespace NFMDL
 
 	void XashModelFileWriter::ConvertSourceFile()
 	{
+		Convert(m_InModelFile->Header, m_OutModelFile->Header);
+
+		CopyElementArray(m_InModelFile->Bones, m_OutModelFile->Bones);
+		CopyElementArray(m_InModelFile->BoneControllers, m_OutModelFile->BoneControllers);
+		CopyElementArray(m_InModelFile->HitBoxes, m_OutModelFile->HitBoxes);
+		CopyElementArray(m_InModelFile->Sequences, m_OutModelFile->Sequences);
+		CopyElementArray(m_InModelFile->SequenceGroups, m_OutModelFile->SequenceGroups);
+		CopyElementArray(m_InModelFile->Attachments, m_OutModelFile->Attachments);
+
+		m_OutModelFile->Skins = m_InModelFile->Skins;
+		m_OutModelFile->Events = m_InModelFile->Events;
+		m_OutModelFile->FootPivots = m_InModelFile->FootPivots;
+		m_OutModelFile->AnimationData = m_InModelFile->AnimationData;
+
+		// TODO: The rest
 	}
 
 	void XashModelFileWriter::WriteEntireFile()
 	{
+		// TODO
 	}
 }
