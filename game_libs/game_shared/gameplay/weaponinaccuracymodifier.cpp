bfc98531ede978eff82eca27824cba4c24533c70
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2021-01-17 18:53:37 +0000

    Created weapon inaccuracy modifier class

---
+++ game_libs/game_shared/gameplay/weaponinaccuracymodifier.cpp
@@ -0,0 +1,146 @@
+#include "weaponinaccuracymodifier.h"
+#include "miniutl.h"
+
+namespace
+{
+	static inline float Clamp(float min, float val, float max)
+	{
+		return Max(Min(max, val), min);
+	}
+
+	// Clamps in range [0 1].
+	static inline float ClampNormalised(float val)
+	{
+		return Clamp(0.0f, val, 1.0f);
+	}
+}
+
+CWeaponInaccuracyModifier::CWeaponInaccuracyModifier()
+{
+	Clear();
+}
+
+void CWeaponInaccuracyModifier::Clear()
+{
+	ResetInaccuracy();
+
+	m_WeaponFireImpulse = 0.0f;
+	m_WeaponFiredThisFrame = false;
+	m_WeaponFireTime = 0.0f;
+	m_WeaponFollowCoefficient = 0.5f;
+	m_StandardFollowCoefficient = 0.95f;
+	m_FollowCoeffInterpFunc = FollowCoefficientInterpFunction();
+}
+
+void CWeaponInaccuracyModifier::ResetInaccuracy()
+{
+	m_Inaccuracy = 0.0f;
+	m_Time = 0.0f;
+}
+
+float CWeaponInaccuracyModifier::CurrentInaccuracy() const
+{
+	return m_Inaccuracy;
+}
+
+void CWeaponInaccuracyModifier::RecalculateInaccuracy(float trueInaccuracy, float dt)
+{
+	// Add impulse to current inaccuracy if the player's weapon was fired this frame.
+	if ( m_WeaponFiredThisFrame )
+	{
+		m_Inaccuracy += m_WeaponFireImpulse;
+		m_WeaponFireTime = m_Time;
+		m_WeaponFiredThisFrame = false;
+	}
+
+	const float timeSinceWeaponFired = Max(0.0f, m_Time - m_WeaponFireTime);
+
+	// Get interpolation value between both follow coefficients.
+	// If interp == 1, the weapon's follow coefficient is used.
+	// If interp == 0, the standard follow coefficient is used.
+	// If interp is between 0 and 1, different proportions of each
+	// coefficient are used.
+	const float interp = m_FollowCoeffInterpFunc
+		? ClampNormalised(m_FollowCoeffInterpFunc(timeSinceWeaponFired))
+		: ClampNormalised(DefaultFollowCoefficientInterp(timeSinceWeaponFired));
+
+	// Calculate the follow coefficient we will use.
+	const float followCoefficient = (interp * m_WeaponFollowCoefficient) + ((1 - interp) * m_StandardFollowCoefficient);
+
+	// Move current inaccuracy closer to the true inaccuracy curve.
+	// How influential the true inaccuracy is depends on the follow coefficient.
+	// If this is 1, the true inaccuracy curve is followed accurately.
+	// If it is 0, the true inaccuracy curve has no influence.
+	m_Inaccuracy += followCoefficient * (trueInaccuracy - m_Inaccuracy);
+
+	// Clamp inaccuracy to valid range.
+	m_Inaccuracy = Clamp(0.0f, m_InaccuracyCap, m_InaccuracyCap);
+
+	// Advance internal time counter.
+	m_Time += dt;
+}
+
+float CWeaponInaccuracyModifier::InaccuracyCap() const
+{
+	return m_InaccuracyCap;
+}
+
+void CWeaponInaccuracyModifier::SetInaccuracyCap(float cap)
+{
+	m_InaccuracyCap = Max(cap, 0.0f);
+}
+
+float CWeaponInaccuracyModifier::WeaponFireImpulse() const
+{
+	return m_WeaponFireImpulse;
+}
+
+void CWeaponInaccuracyModifier::SetWeaponFireImpulse(float impulse)
+{
+	m_WeaponFireImpulse = impulse;
+}
+
+bool CWeaponInaccuracyModifier::WeaponFiredThisFrame() const
+{
+	return m_WeaponFiredThisFrame;
+}
+
+void CWeaponInaccuracyModifier::SetWeaponFiredThisFrame()
+{
+	m_WeaponFiredThisFrame = true;
+}
+
+float CWeaponInaccuracyModifier::WeaponFollowCoefficient() const
+{
+	return m_WeaponFollowCoefficient;
+}
+
+void CWeaponInaccuracyModifier::SetWeaponFollowCoefficient(float coeff)
+{
+	m_WeaponFollowCoefficient = ClampNormalised(coeff);
+}
+
+float CWeaponInaccuracyModifier::StandardFollowCoefficient() const
+{
+	return m_StandardFollowCoefficient;
+}
+
+void CWeaponInaccuracyModifier::SetStandardFollowCoefficient(float coeff)
+{
+	m_StandardFollowCoefficient = ClampNormalised(coeff);
+}
+
+void CWeaponInaccuracyModifier::SetFollowCoefficientInterpFunction(const FollowCoefficientInterpFunction& func)
+{
+	m_FollowCoeffInterpFunc = func;
+}
+
+void CWeaponInaccuracyModifier::ClearFollowCoefficientInterpFunction()
+{
+	m_FollowCoeffInterpFunc = FollowCoefficientInterpFunction();
+}
+
+float CWeaponInaccuracyModifier::DefaultFollowCoefficientInterp(float timeSinceWeaponFired)
+{
+	return 1.0f - timeSinceWeaponFired;
+}
---
+++ game_libs/game_shared/gameplay/weaponinaccuracymodifier.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <functional>
+
+class CWeaponInaccuracyModifier
+{
+public:
+	using FollowCoefficientInterpFunction = std::function<float(float)>;
+
+	CWeaponInaccuracyModifier();
+
+	// Resets all properties and internal values.
+	void Clear();
+
+	// Resets inaccuracy and accumulated time, but leaves other properties unaffected.
+	void ResetInaccuracy();
+
+	// Returns the current inaccuracy value, as determined by the last call to RecalculateInaccuracy().
+	float CurrentInaccuracy() const;
+
+	// Recalculates the inaccuracy value based on the true value for this frame,
+	// and the amount of time that has passed since the last call.
+	// The true value should be calculated based on the player's movement and
+	// rotation speed. The computed inaccuracy value will track this value across
+	// frames, based on the standard follow coefficient, and a weapon being fired
+	// will apply an impulse that temporarily increases the inaccuracy.
+	void RecalculateInaccuracy(float trueInaccuracy, float dt);
+
+	// The maximum value that the inaccuracy is allowed to reach.
+	// If it exceeds this value, it will be clamped.
+	float InaccuracyCap() const;
+	void SetInaccuracyCap(float cap);
+
+	// The absolute value added to the inaccuracy on the frame when a weapon is fired.
+	float WeaponFireImpulse() const;
+	void SetWeaponFireImpulse(float impulse);
+
+	// If set, the weapon is considered to have been fired when RecalculateInaccuracy()
+	// is next called.
+	bool WeaponFiredThisFrame() const;
+	void SetWeaponFiredThisFrame();
+
+	// Follow coefficient that is used if the weapon has recently been fired.
+	// This should be less than the standard coefficient, as if the weapon has recently
+	// been fired, the calculated accuracy curve should remain above the true curve
+	// for a short period of time.
+	float WeaponFollowCoefficient() const;
+	void SetWeaponFollowCoefficient(float coeff);
+
+	// Follow coefficient that is used if the weapon has not recently been fired.
+	// This should be closer to 1 than the weapon follow coefficient, as the longer
+	// the time since the weapon last fired, the more closely the calculated
+	// inaccuracy curve should track the true curve.
+	float StandardFollowCoefficient() const;
+	void SetStandardFollowCoefficient(float coeff);
+
+	// Function that returns a value in the interval [0 1] based on how long it has
+	// been since the weapon was fired. The argument to the function is the time in
+	// seconds since the weapon was fired, and will always be >= 0.
+	// If 1 is returned, the weapon follow coefficient is used to track the true
+	// inaccuracy curve; if 0 is returned, the standard follow coefficient is used.
+	// Any value in-between implies a linear interpolation between these two coefficients.
+	// If this callback is left blank, the default interp function ramps linearly from
+	// 1 to 0 over a period of 1s after the weapon is fired.
+	void SetFollowCoefficientInterpFunction(const FollowCoefficientInterpFunction& func);
+	void ClearFollowCoefficientInterpFunction();
+
+private:
+	float DefaultFollowCoefficientInterp(float timeSinceWeaponFired);
+
+	float m_Inaccuracy = 0.0f;
+	float m_InaccuracyCap = 1.0f;
+	float m_Time = 0.0f;
+
+	float m_WeaponFireImpulse = 0.0f;
+	bool m_WeaponFiredThisFrame = false;
+	float m_WeaponFireTime = 0.0f;
+	float m_WeaponFollowCoefficient = 0.5f;
+	float m_StandardFollowCoefficient = 0.95f;
+	FollowCoefficientInterpFunction m_FollowCoeffInterpFunc;
+};
